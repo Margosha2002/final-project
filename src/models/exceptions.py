18a704120170dbb6f0d5760ebdcdc90e65585ea1
LesyaTkachuk <lesyalesya9876@gmail.com>
2023-11-01 20:21:56 +0200

    move all exceptions into separate file

---
+++ src/models/exceptions.py
@@ -0,0 +1,25 @@
+class NameIsRequiredException(Exception):
+    pass
+
+
+class PhoneIsRequiredException(Exception):
+    pass
+
+
+class InvalidDaysCount(Exception):
+    pass
+
+
+class InvalidChangeField(Exception):
+    pass
+
+class PhoneValidationError(Exception):
+    pass
+
+
+class BirthdayValidationError(Exception):
+    pass
+
+
+class EmailValidationError(Exception):
+    pass
